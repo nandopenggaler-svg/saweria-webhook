b1bb9bdc5e08e0a59b05a3120e84ec6b27df8c5f
nandopenggaler-svg <nandopenggaler@gmail.com>
2026-06-07 11:53:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Webhook Aktif");
+
+    res.json({
+        status: "online",
+        universeId: process.env.UNIVERSE_ID,
+        datastore: process.env.DATASTORE_NAME,
+        apiKeyExists: !!process.env.ROBLOX_API_KEY
+    });
+
 });
 
 app.post("/saweria", (req, res) => {
+
     console.log("DONASI MASUK");
     console.log(req.body);
+
+    console.log("Universe:", process.env.UNIVERSE_ID);
+    console.log("DataStore:", process.env.DATASTORE_NAME);
+    console.log("API Key Ada:", !!process.env.ROBLOX_API_KEY);
 
     res.status(200).send("OK");
 });
